0338a8f90c794dff206d70a59ba3f2b5978f91ce
srdgz <sandra.rr@icloud.com>
2024-10-15 18:21:51 +0200

    feat: add new favicon and title in index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        // Definir colores para el tema claro
+        primary: {
+          light: "#4ade80",
+          DEFAULT: "#16a34a",
+          dark: "#166534",
+        },
+        secondary: {
+          light: "#60a5fa",
+          DEFAULT: "#3b82f6",
+          dark: "#1e3a8a",
+        },
+        // Definir colores para el tema oscuro
+        dark: {
+          primary: {
+            light: "#8b5cf6",
+            DEFAULT: "#7c3aed",
+            dark: "#6b21a8",
+          },
+          secondary: {
+            light: "#fbbf24",
+            DEFAULT: "#f59e0b",
+            dark: "#b45309",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
